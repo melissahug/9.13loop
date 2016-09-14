7191cacc21498fe12dd988f3ad955b80414fe5b2
Melissa <hughes.melissa1@gmail.com>
2016-09-14 09:50:23 -0400

    corrected to be an actual loop

--- Program.cs
+++ Program.cs
@@ -8,26 +8,29 @@ namespace homework_9._13_loop
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args)                                  
+           {
 
-            //bool executeLoop = true;  The boolean is not needed
-            int userInput = int.Parse(Console.ReadLine());
-            //executeLoop = userInput % 3 != 0;
-
-            // while userInput is NOT divisible by 3, the loop will continue.
-            while (userInput % 3 != 0)                      //condition
+            
+            while (true)                                //changed the coding to be an actual loop.9
+            
             {
-                Console.WriteLine(" You Lost! ");           //met condition and writing you lost
-                userInput = int.Parse(Console.ReadLine());  // gets user input and starts with the condition
-            }
+                 
+                int userInput = int.Parse(Console.ReadLine());
 
-            if (userInput % 3 == 0)                         // unless the userInput IS divisible by three.. 
-            {
-                Console.WriteLine(" You Win");              // prints this line. No other commands after.
-                                                            // since there is no userInput, it won't create a loop (guessing)
-            }
-                    
+
+                if (userInput % 3 != 0)
+                {
+                    Console.WriteLine(" You Lost! ");                          
+                                                                              
+                }
+                
+              else if (userInput % 3 == 0)                                     
+                {
+                    Console.WriteLine(" You Win");                            
+                    break;                                                    
+                }
+            }      
           }
     }
 }
